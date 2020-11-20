7b6c09e42bbd7468cb58802ca7d59192e87dbade
Joseph Anderson <fitnycdigitalinitiatives@users.noreply.github.com>
2020-11-20 16:22:07 -0500

    section layout and lotsa tinkering

--- common/footer.php
+++ common/footer.php
@@ -5,7 +5,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@barba/core"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/CSSRulePlugin.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/openseadragon.min.js" integrity="sha384-BqvbWCNWGAf21sDh6X5DGseJPJ+iNSRIX/j6rxssCsNw1dbPRaX8TiA9gfy3Jd2F" crossorigin="anonymous"></script>
     <script src="<?php echo src('owl.carousel.min', 'javascripts/owl', 'js'); ?>"></script>
     <script src="<?php echo src('beller', 'javascripts', 'js'); ?>"></script>
