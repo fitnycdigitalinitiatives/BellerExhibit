85f25d8b2c612a6389887593d6699f964571745c
Joseph Anderson <joseph_anderson@fitnyc.edu>
2025-05-30 15:12:28 -0400

    s3

--- common/footer.php
+++ common/footer.php
@@ -6,6 +6,10 @@
     <script src="https://cdn.jsdelivr.net/npm/@barba/core@2.9.7/dist/barba.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/openseadragon.min.js" integrity="sha384-BqvbWCNWGAf21sDh6X5DGseJPJ+iNSRIX/j6rxssCsNw1dbPRaX8TiA9gfy3Jd2F" crossorigin="anonymous"></script>
+    <script>
+      const settingsIiifEndpoint = "<?php echo get_theme_option('iiif_endpoint'); ?>";
+    </script>
     <script src="<?php echo src('beller', 'javascripts', 'js'); ?>"></script>
-  </body>
-</html>
+    </body>
+
+    </html>
--- javascripts/beller.js
+++ javascripts/beller.js
@@ -1,6 +1,6 @@
 function init() {
   //Scroll animation and offset
-  $('a.scroll-link').on("click", function(event) {
+  $('a.scroll-link').on("click", function (event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
       scrollTop: ($($anchor.attr('href')).offset().top + 0)
@@ -9,12 +9,10 @@ function init() {
   });
   // end scroll animation
   //open seadragon //
-  $('.openseadragon').each(function() {
+  $('.openseadragon').each(function () {
     var currentViewer = $(this);
     var currentViewerID = currentViewer.attr('id');
-    var recordID = $(this).data('record_id');
-    var recordName = $(this).data('record_name');
-    var iiifEndpoint = 'https://fitdil.fitnyc.edu/media/iiif/' + recordID + '/' + recordName + '/info.json';
+    var iiifEndpoint = $(this).data('iiif-endpoint');
     var viewer = OpenSeadragon({
       id: currentViewerID,
       prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
@@ -25,22 +23,20 @@ function init() {
       controlsFadeDelay: 1000,
       tileSources: iiifEndpoint
     });
-    viewer.world.addHandler('add-item', function(event) {
+    viewer.world.addHandler('add-item', function (event) {
       var tiledImage = event.item;
-      tiledImage.addHandler('fully-loaded-change', function() {
+      tiledImage.addHandler('fully-loaded-change', function () {
         $(currentViewer).parent().children('.loader').remove();
       });
     });
   });
 
-  $('.openseadragon-popup').click(function() {
+  $('.openseadragon-popup').click(function () {
     var currentViewer = $(this);
     var currentViewerID = currentViewer.attr('id');
     var seadragon_frame = $('<div class="openseadragon-full" id="' + currentViewerID + '-frame"><div class="loader"></div></div>');
     $(this).append(seadragon_frame);
-    var recordID = $(this).data('record_id');
-    var recordName = $(this).data('record_name');
-    var iiifEndpoint = 'https://fitdil.fitnyc.edu/media/iiif/' + recordID + '/' + recordName + '/info.json';
+    var iiifEndpoint = $(this).data('iiif-endpoint');
     var viewer = OpenSeadragon({
       id: currentViewerID + '-frame',
       prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
@@ -51,23 +47,23 @@ function init() {
       controlsFadeDelay: 1000,
       tileSources: iiifEndpoint
     });
-    viewer.setFullScreen(true).addHandler('full-screen', function(data) {
+    viewer.setFullScreen(true).addHandler('full-screen', function (data) {
       if (!data.fullScreen) {
-        setTimeout(function() {
+        setTimeout(function () {
           viewer.destroy();
         }, 300);
       };
     });
-    viewer.world.addHandler('add-item', function(event) {
+    viewer.world.addHandler('add-item', function (event) {
       var tiledImage = event.item;
-      tiledImage.addHandler('fully-loaded-change', function() {
+      tiledImage.addHandler('fully-loaded-change', function () {
         $('.loader').remove();
       });
     });
   });
   // end openseadragon
   // offcanvas navbar
-  $('[data-toggle="offcanvas"]').on('click', function() {
+  $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open');
     if ($('.offcanvas-collapse').hasClass('open')) {
       $('[data-toggle="offcanvas"]').attr('aria-expanded', 'true');
@@ -76,7 +72,7 @@ function init() {
     }
   });
   //fix modals
-  $('.modal').each(function() {
+  $('.modal').each(function () {
     $(this).attr({
       "data-barba-prevent": "all"
     }).appendTo("body");
@@ -84,13 +80,13 @@ function init() {
   $('.card-footer').attr({
     "data-barba-prevent": "all"
   });
-  $('.modal-header').each(function() {
+  $('.modal-header').each(function () {
     var listItems = $(this).children();
     $(this).append(listItems.get().reverse());
   });
   //fix FITting Room nav
   if (document.getElementsByTagName("title")[0].innerHTML.includes("FITting Room")) {
-    $('.info').each(function() {
+    $('.info').each(function () {
       var footer = $(this).parent();
       var nav = footer.find('.nav-pills');
       var new_div = $("<li class='nav-item'></li>");
@@ -103,7 +99,7 @@ function init() {
     var collection = $('#collection-api').data('collection-id');
     var index = 1;
     loadCollection(collection, index);
-    $('#load-collection').click(function() {
+    $('#load-collection').click(function () {
       index++;
       loadCollection(collection, index);
     });
@@ -111,37 +107,38 @@ function init() {
 }
 
 function loadCollection(collection, index) {
-  $.getJSON("/api/items?collection=" + collection + "&page=" + index + "&per_page=30", function(data, status, xhr) {
+  $.getJSON("/api/items?collection=" + collection + "&page=" + index + "&per_page=30", function (data, status, xhr) {
     var total_results = xhr.getResponseHeader("Omeka-Total-Results");
     var index_max = Math.ceil(total_results / 30);
     if (index_max == index) {
       $('#load-collection').attr("disabled", true)
     }
-    $.each(data, function(i, item) {
+    $.each(data, function (i, item) {
       element_texts = item.element_texts;
       var item_id = item.id;
-      var record_name = null;
-      var record_id = null;
+      var s3_path = null;
       var title = '';
       for (var i = 0; i < element_texts.length; i++) {
-        if (element_texts[i].element.name == "Record Name") {
-          record_name = element_texts[i].text
-        }
-        if (element_texts[i].element.name == "Record ID") {
-          record_id = element_texts[i].text
+        if (element_texts[i].element.name == "s3_path") {
+          s3_path = element_texts[i].text
         }
         if (element_texts[i].element.name == "Title") {
           title = element_texts[i].text
         }
       }
-      if (record_name && record_id) {
-        var image_url = 'https://fitdil.fitnyc.edu/media/get/' + record_id + '/' + record_name + '/400x400/';
+      if (s3_path && settingsIiifEndpoint) {
+        const lastIndex = s3_path.lastIndexOf('/');
+        var image_url = s3_path.substring(0, lastIndex).replace("/objects", "/thumbnails") + '/' + s3_path.substring(lastIndex + 1).substring(0, 36) + '.jpg';
+        const url = new URL(s3_path);
+        let key = url.pathname.slice(1);
+
+        let iiifEndpoint = settingsIiifEndpoint + key.slice(0, -4).replace("/", "%2F") + "/info.json";
         var card = `
           <div class="col">
             <div class="card mb-2">
-              <img class="card-img-top openseadragon-popup" id="openseadragon` + record_id + `" src="` + image_url + `" alt="` + title + `" data-record_id="` + record_id + `" data-record_name="` + record_name + `" />
+              <img class="card-img-top openseadragon-popup" id="openseadragon` + item_id + `" src="` + image_url + `" alt="` + title + `" data-item_id="` + item_id + `" data-iiif-endpoint="` + iiifEndpoint + `" />
               <div class="card-footer gallery-caption">
-              <button class="info" type="button" name="button" data-toggle="modal" data-target="#record-modal` + record_id + `">
+              <button class="info" type="button" name="button" data-toggle="modal" data-target="#record-modal` + item_id + `">
                 <i class="fas fa-info-circle"></i><span class="sr-only">Information</span>
               </button>
               </div>
@@ -149,11 +146,11 @@ function loadCollection(collection, index) {
           </div>
         `;
         var modal = `
-        <div class="modal fade" id="record-modal` + record_id + `" tabindex="-1" role="dialog" aria-labelledby="myModalLabel` + record_id + `" data-barba-prevent="all">
+        <div class="modal fade" id="record-modal` + item_id + `" tabindex="-1" role="dialog" aria-labelledby="myModalLabel` + item_id + `" data-barba-prevent="all">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-              <span class="modal-title font-weight-bold" id="myModalLabel` + record_id + `">Item Information</span>
+              <span class="modal-title font-weight-bold" id="myModalLabel` + item_id + `">Item Information</span>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
               </div>
               <div class="modal-body">
@@ -174,14 +171,12 @@ function loadCollection(collection, index) {
     });
     $('#load-collection').removeClass('d-none');
     //redo OpenSeadragon
-    $('.openseadragon-popup').click(function() {
+    $('.openseadragon-popup').click(function () {
       var currentViewer = $(this);
       var currentViewerID = currentViewer.attr('id');
       var seadragon_frame = $('<div class="openseadragon-full" id="' + currentViewerID + '-frame"><div class="loader"></div></div>');
       $(this).append(seadragon_frame);
-      var recordID = $(this).data('record_id');
-      var recordName = $(this).data('record_name');
-      var iiifEndpoint = 'https://fitdil.fitnyc.edu/media/iiif/' + recordID + '/' + recordName + '/info.json';
+      var iiifEndpoint = $(this).data('iiif-endpoint');
       var viewer = OpenSeadragon({
         id: currentViewerID + '-frame',
         prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
@@ -192,125 +187,125 @@ function loadCollection(collection, index) {
         controlsFadeDelay: 1000,
         tileSources: iiifEndpoint
       });
-      viewer.setFullScreen(true).addHandler('full-screen', function(data) {
+      viewer.setFullScreen(true).addHandler('full-screen', function (data) {
         if (!data.fullScreen) {
-          setTimeout(function() {
+          setTimeout(function () {
             viewer.destroy();
           }, 300);
         };
       });
-      viewer.world.addHandler('add-item', function(event) {
+      viewer.world.addHandler('add-item', function (event) {
         var tiledImage = event.item;
-        tiledImage.addHandler('fully-loaded-change', function() {
+        tiledImage.addHandler('fully-loaded-change', function () {
           $('.loader').remove();
         });
       });
     });
   });
 }
-$(document).ready(function() {
+$(document).ready(function () {
   init();
   barba.init({
     transitions: [{
-        name: 'slide-transition',
-        leave(data) {
-          //need to clear out modals so they don't get carried from page to page
-          $('body').children('.modal').remove();
-          if (data.trigger.className.includes("previous") || data.trigger.className.includes("navbar-brand")) {
-            return gsap.to(data.current.container, {
-              xPercent: 100,
-              duration: .75
-            });
-          } else if (data.trigger.className.includes("nav-link")) {
-            $('.offcanvas-collapse').toggleClass('open');
-            return gsap.to(data.current.container, {
-              yPercent: -100,
-              duration: .75,
-              delay: .5
-            });
-          } else {
-            return gsap.to(data.current.container, {
-              xPercent: -100,
-              duration: .75
-            });
-          }
-        },
-        enter(data) {
-          // need to scroll up on mobile because page height isn't fixed
-          if (window.matchMedia('(max-width: 767px)').matches) {
-            $("html, body").animate({
-              scrollTop: 0
-            }, "slow");
-          }
-          if (data.trigger.className.includes("previous") || data.trigger.className.includes("navbar-brand")) {
-
-            return gsap.from(data.next.container, {
-              xPercent: -100,
-              duration: .75
-            });
-          } else if (data.trigger.className.includes("nav-link")) {
-            return gsap.from(data.next.container, {
-              yPercent: 100,
-              duration: .75,
-            });
-          } else {
-
-            return gsap.from(data.next.container, {
-              xPercent: 100,
-              duration: .75
-            });
-          }
+      name: 'slide-transition',
+      leave(data) {
+        //need to clear out modals so they don't get carried from page to page
+        $('body').children('.modal').remove();
+        if (data.trigger.className.includes("previous") || data.trigger.className.includes("navbar-brand")) {
+          return gsap.to(data.current.container, {
+            xPercent: 100,
+            duration: .75
+          });
+        } else if (data.trigger.className.includes("nav-link")) {
+          $('.offcanvas-collapse').toggleClass('open');
+          return gsap.to(data.current.container, {
+            yPercent: -100,
+            duration: .75,
+            delay: .5
+          });
+        } else {
+          return gsap.to(data.current.container, {
+            xPercent: -100,
+            duration: .75
+          });
         }
       },
-      {
-        name: 'launch',
-        once(data) {
-          // only run if it starts on summary page
-          if ($('#main').data('barba-namespace') == 'exhibits summary') {
-            let tl = gsap.timeline(); //create the timeline
-            return tl.from('#exhibit-landing', {
-                yPercent: -100,
-                duration: .75,
-                delay: 1
-              })
-              .from('#summary-block', {
-                xPercent: -150,
-                duration: .5,
-                ease: "back.out(1.7)"
-              });
-          }
+      enter(data) {
+        // need to scroll up on mobile because page height isn't fixed
+        if (window.matchMedia('(max-width: 767px)').matches) {
+          $("html, body").animate({
+            scrollTop: 0
+          }, "slow");
         }
-      },
-      //special case because going back to landing messes up slider
-      {
-        name: 'landing',
-        to: {
-          namespace: [
-            'exhibits summary'
-          ],
-        },
-        enter(data) {
-          // need to scroll up on mobile because page height isn't fixed
-          if (window.matchMedia('(max-width: 767px)').matches) {
-            $("html, body").animate({
-              scrollTop: 0
-            }, "slow");
-          }
-          if (data.trigger.className.includes("previous") || data.trigger.className.includes("navbar-brand") || data.trigger.className.includes("nav-link")) {
+        if (data.trigger.className.includes("previous") || data.trigger.className.includes("navbar-brand")) {
 
-            return gsap.from(data.next.container, {
-              xPercent: -100,
-              duration: .75
-            });
-          } else {
+          return gsap.from(data.next.container, {
+            xPercent: -100,
+            duration: .75
+          });
+        } else if (data.trigger.className.includes("nav-link")) {
+          return gsap.from(data.next.container, {
+            yPercent: 100,
+            duration: .75,
+          });
+        } else {
 
-            return gsap.from(data.next.container, {
-              xPercent: 100,
-              duration: .75
-            });
-          }
+          return gsap.from(data.next.container, {
+            xPercent: 100,
+            duration: .75
+          });
         }
       }
+    },
+    {
+      name: 'launch',
+      once(data) {
+        // only run if it starts on summary page
+        if ($('#main').data('barba-namespace') == 'exhibits summary') {
+          let tl = gsap.timeline(); //create the timeline
+          return tl.from('#exhibit-landing', {
+            yPercent: -100,
+            duration: .75,
+            delay: 1
+          })
+            .from('#summary-block', {
+              xPercent: -150,
+              duration: .5,
+              ease: "back.out(1.7)"
+            });
+        }
+      }
+    },
+    //special case because going back to landing messes up slider
+    {
+      name: 'landing',
+      to: {
+        namespace: [
+          'exhibits summary'
+        ],
+      },
+      enter(data) {
+        // need to scroll up on mobile because page height isn't fixed
+        if (window.matchMedia('(max-width: 767px)').matches) {
+          $("html, body").animate({
+            scrollTop: 0
+          }, "slow");
+        }
+        if (data.trigger.className.includes("previous") || data.trigger.className.includes("navbar-brand") || data.trigger.className.includes("nav-link")) {
+
+          return gsap.from(data.next.container, {
+            xPercent: -100,
+            duration: .75
+          });
+        } else {
+
+          return gsap.from(data.next.container, {
+            xPercent: 100,
+            duration: .75
+          });
+        }
+      }
+    }
     ]
   });
   barba.hooks.after(() => {
